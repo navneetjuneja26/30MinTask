215bfdca3806476c1e9ced43e6b9804a48a83890
navneetjuneja26 <navneetjuneja26@gmail.com>
2019-02-11 01:17:46 +0530

    Added Controllers and Models

---
+++ navneet 004 tic tac toe/TicTacToe/TicTacToe/Controller/PeopleController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controller
+{
+    public class PeopleController : ApiController
+    {
+        private TicTacToeEntities2 db = new TicTacToeEntities2();
+
+        // GET: api/People
+        public IQueryable<Person> GetPeople()
+        {
+            return db.People;
+        }
+
+        // GET: api/People/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult GetPerson(string id)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/People/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPerson(string id, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.userid)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/People
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult PostPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.People.Add(person);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PersonExists(person.userid))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = person.userid }, person);
+        }
+
+        // DELETE: api/People/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult DeletePerson(string id)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            db.People.Remove(person);
+            db.SaveChanges();
+
+            return Ok(person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonExists(string id)
+        {
+            return db.People.Count(e => e.userid == id) > 0;
+        }
+    }
+}
